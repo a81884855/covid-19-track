5c18e50b7e97c8660ed5176ac010dd9c73f63f68
Gary <garyguan0713@gmail.com>
2020-05-10 20:47:30 -0700

    implement navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
 import ReactTooltip from "react-tooltip";
+import { Row, Col } from "react-bootstrap";
 
 import "./App.css";
 
+import Nav from "./Layout/Navbar";
 import WorldMapChart from "./WorldMap";
 
 function App() {
   const [content, setContent] = useState("");
   return (
-    <div
-      style={{
-        margin: 50,
-      }}
-    >
-      <WorldMapChart setTooltipContent={setContent} />
-      <ReactTooltip>{content}</ReactTooltip>
-    </div>
+    <>
+      <Nav />
+      <Row>
+        <Col
+          xs={12}
+          md={8}
+          style={{
+            padding: "0 20px 0 20px",
+          }}
+        >
+          <WorldMapChart setTooltipContent={setContent} />
+          <ReactTooltip>{content}</ReactTooltip>
+        </Col>
+        <Col xs={12} md={4}>
+          123
+        </Col>
+      </Row>
+    </>
   );
 }
 
