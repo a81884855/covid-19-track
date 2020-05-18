14462a3fa2e78ed967dba8ea80f19c9197ad6d54
Gary <garyguan0713@gmail.com>
2020-05-17 21:54:21 -0700

    add scroll to end function

--- src/caseNumBoard/index.js
+++ src/caseNumBoard/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, lazy, Suspense } from "react";
 // import axios from "axios";
 import { Table, Spinner, Button } from "react-bootstrap";
 import { abbrev, rounded } from "../helper";
@@ -21,6 +21,10 @@ const Board = () => {
             height: 470,
             overflow: "scroll",
           }}
+          onScroll={(e) => {
+            if (e.target.scrollHeight - e.target.scrollTop === 470)
+              setMaxCount(maxCount + 20);
+          }}
         >
           <Table striped borderless hover>
             <thead>
@@ -28,41 +32,82 @@ const Board = () => {
                 <th>#</th>
                 <th>Country</th>
                 <th>Case #</th>
+                <th>Death</th>
+                <th>Recovered</th>
               </tr>
             </thead>
             <tbody>
               <tr></tr>
               {caseData
                 .slice(0, maxCount)
-                .map(({ Country, TotalConfirmed, NewConfirmed }, i) => (
-                  <tr key={Country}>
-                    <td>{i + 1}</td>
-                    <td>{abbrev(Country)}</td>
-                    <td>
-                      {TotalConfirmed}{" "}
-                      <div
-                        style={{
-                          fontSize: "0.6rem",
-                          color: "#427cd2",
-                          fontWeight: "bold",
-                        }}
-                      >
-                        {typeof NewConfirmed === "number"
-                          ? ` + ${rounded(NewConfirmed)} new`
-                          : "N/A"}
-                      </div>
-                    </td>
-                  </tr>
-                ))}
+                .map(
+                  (
+                    {
+                      Country,
+                      TotalConfirmed,
+                      NewConfirmed,
+                      NewDeaths,
+                      TotalDeaths,
+                      NewRecovered,
+                      TotalRecovered,
+                    },
+                    i
+                  ) => (
+                    <tr key={Country}>
+                      <td>{i + 1}</td>
+                      <td>{abbrev(Country)}</td>
+                      <td className="confirmedCase">
+                        {TotalConfirmed}{" "}
+                        <div
+                          className="font-weight-bold"
+                          style={{
+                            fontSize: "0.6rem",
+                          }}
+                        >
+                          {typeof NewConfirmed === "number"
+                            ? ` + ${rounded(NewConfirmed)} new`
+                            : "N/A"}
+                        </div>
+                      </td>
+                      <td className="deathCase">
+                        {TotalDeaths}{" "}
+                        <div
+                          className="font-weight-bold"
+                          style={{
+                            fontSize: "0.6rem",
+                          }}
+                        >
+                          {typeof NewConfirmed === "number"
+                            ? ` + ${rounded(NewDeaths)} new`
+                            : "N/A"}
+                        </div>
+                      </td>
+                      <td className="recoveredCase">
+                        {TotalRecovered}{" "}
+                        <div
+                          className="font-weight-bold"
+                          style={{
+                            fontSize: "0.6rem",
+                          }}
+                        >
+                          {typeof NewConfirmed === "number"
+                            ? ` + ${rounded(NewRecovered)} new`
+                            : "N/A"}
+                        </div>
+                      </td>
+                    </tr>
+                  )
+                )}
             </tbody>
           </Table>
-          <Button
+          <Suspense fallback={<h1>Loading…</h1>}></Suspense>
+          {/* <Button
             variant="info"
             onClick={() => setMaxCount(maxCount + 20)}
             block
           >
             More...
-          </Button>
+          </Button> */}
         </div>
       ) : (
         <div
