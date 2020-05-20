3b33c320ab3fd64d63bbddfcfbd98996de5c0ef7
Gary <garyguan0713@gmail.com>
2020-05-19 22:27:09 -0700

    implement container

--- src/Map/index.js
+++ src/Map/index.js
@@ -1,78 +1,32 @@
-import React, { memo, useState, useContext, useEffect } from "react";
+import React from "react";
 
 import "./index.css";
-import { Context as WorldMapContext } from "../context/worldMapContext";
+import { Row, Col } from "react-bootstrap";
+import MapChart from "./MapChart";
+import Board from "../caseNumBoard";
 
-import MapHeadDisplay from "./statisticsDisplay";
-
-import WorldMapChart from "./WolrdMapChart";
-import USMapChart from "./USMapChart";
-
-import Tabs from "../Component/Tabs";
-
-const MapChart = () => {
-  const [loading, setLoading] = useState(false);
-  const [map, setMap] = useState("World");
-
-  const {
-    state: { worldMapData, Global },
-    addData,
-    fetchData,
-  } = useContext(WorldMapContext);
-
-  useEffect(() => {
-    let fetch = async () => {
-      try {
-        let data = await fetchData();
-        await addData(data);
-        setLoading(true);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    fetch();
-  }, []);
-
-  const mapSwitch = (map) => {
-    switch (map) {
-      case "World":
-        return (
-          <WorldMapChart
-            loading={loading}
-            worldMapData={worldMapData}
-            GlobalTotalConfirmed={Global.TotalConfirmed}
-          />
-        );
-      case "United States":
-        return <USMapChart />;
-      default:
-        return (
-          <WorldMapChart
-            loading={loading}
-            worldMapData={worldMapData}
-            GlobalTotalConfirmed={Global.TotalConfirmed}
-          />
-        );
-    }
-  };
-
-  console.log(map);
+const MapContainer = () => {
   return (
-    <>
-      <Tabs setMap={setMap} />
-      <div
+    <div
+      style={{
+        margin: "0.3rem 1rem",
+        padding: "0 1rem",
+      }}
+    >
+      <Row
         style={{
-          border: "1px solid black",
-          borderRadius: "20px",
-          overflow: "auto",
+          margin: "0 0 60px 0",
         }}
       >
-        {mapSwitch(map)}
-        {MapHeadDisplay(Global)}
-      </div>
-    </>
+        <Col lg={12} xl={8}>
+          <MapChart />
+        </Col>
+        <Col lg={12} xl={4}>
+          <Board />
+        </Col>
+      </Row>
+    </div>
   );
 };
 
-export default memo(MapChart);
+export default MapContainer;
