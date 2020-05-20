2802e7c205431fe8da49b5a4f245eba83a96e61a
Gary <garyguan0713@gmail.com>
2020-05-19 21:37:45 -0700

    use map context

--- src/Map/USMapChart.js
+++ src/Map/USMapChart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { geoCentroid } from "d3-geo";
 import {
   ComposableMap,
@@ -7,10 +7,16 @@ import {
   Marker,
   Annotation,
 } from "react-simple-maps";
+import { rounded, colorPick, colorCalculate } from "../helper";
 
-import allStates from "../Data/USMap.json";
+import { Context as MapContext } from "../context/MapContext";
 
-const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
+import { OverlayTrigger, Popover, PopoverContent } from "react-bootstrap";
+
+import allStates from "../Data/allStates.json";
+
+import MapCaseSummary from "./MapCaseSummary";
+import { MapLoading } from "../Component/MapLoading";
 
 const offsets = {
   VT: [50, -8],
@@ -25,65 +31,208 @@ const offsets = {
 };
 
 const USMapChart = () => {
+  const [loading, setLoading] = useState(false);
+
+  const {
+    state: { USMapData, mapSummaryData },
+    addUSData,
+    fetchUSData,
+  } = useContext(MapContext);
+
+  useEffect(() => {
+    let fetch = async () => {
+      try {
+        let data = await fetchUSData();
+        await addUSData(data);
+        setLoading(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetch();
+  }, []);
+
   return (
-    <ComposableMap projection="geoAlbersUsa">
-      <Geographies geography={geoUrl}>
-        {({ geographies }) => (
-          <>
-            {geographies.map((geo) => (
-              <Geography
-                key={geo.rsmKey}
-                stroke="#FFF"
-                geography={geo}
-                fill="#DDD"
-                style={{
-                  hover: {
-                    fill: "#F53",
-                    stroke: "#adb5bd",
-                    strokeWidth: 0.75,
-                    outline: "none",
-                  },
-                  pressed: {
-                    fill: "#E42",
-                    stroke: "#adb5bd",
-                    strokeWidth: 0.75,
-                    outline: "none",
-                  },
-                }}
-              />
-            ))}
-            {geographies.map((geo) => {
-              const centroid = geoCentroid(geo);
-              const cur = allStates.find((s) => s.val === geo.id);
-              return (
-                <g key={geo.rsmKey + "-name"}>
-                  {cur &&
-                    centroid[0] > -160 &&
-                    centroid[0] < -67 &&
-                    (Object.keys(offsets).indexOf(cur.id) === -1 ? (
-                      <Marker coordinates={centroid}>
-                        <text y="2" fontSize={14} textAnchor="middle">
-                          {cur.id}
-                        </text>
-                      </Marker>
-                    ) : (
-                      <Annotation
-                        subject={centroid}
-                        dx={offsets[cur.id][0]}
-                        dy={offsets[cur.id][1]}
-                      >
-                        <text x={4} fontSize={14} alignmentBaseline="middle">
-                          {cur.id}
-                        </text>
-                      </Annotation>
-                    ))}
-                </g>
-              );
-            })}
-          </>
-        )}
-      </Geographies>
-    </ComposableMap>
+    <>
+      {loading ? (
+        <>
+          <ComposableMap
+            projectionConfig={{ scale: 900 }}
+            width={750}
+            height={500}
+            projection="geoAlbersUsa"
+          >
+            {loading && (
+              <Geographies geography={USMapData}>
+                {({ geographies }) => (
+                  <>
+                    {geographies.map((geo) => {
+                      const {
+                        name,
+                        cases,
+                        todayCases,
+                        deaths,
+                        todayDeaths,
+                        tests,
+                      } = geo.properties;
+
+                      return (
+                        <OverlayTrigger
+                          trigger={["focus", "hover", "click"]}
+                          placement="right"
+                          key={geo.rsmKey}
+                          overlay={
+                            <Popover>
+                              <Popover.Title as="h4">{name}</Popover.Title>
+                              <PopoverContent>
+                                <div>
+                                  <span
+                                    style={{
+                                      fontSize: "0.7rem",
+                                    }}
+                                  >
+                                    Total Confirmed:{" "}
+                                  </span>
+                                  <span className="confirmedCase">
+                                    {rounded(cases)}
+                                  </span>
+                                  <span
+                                    className="confirmedCase font-weight-bold"
+                                    style={{
+                                      fontSize: "0.6rem",
+                                    }}
+                                  >
+                                    {typeof todayCases === "number"
+                                      ? ` + ${rounded(todayCases)} new`
+                                      : "N/A"}
+                                  </span>
+                                </div>
+
+                                <div>
+                                  <span
+                                    style={{
+                                      fontSize: "0.7rem",
+                                    }}
+                                  >
+                                    Total Deaths:{" "}
+                                  </span>
+                                  <span className="deathCase">
+                                    {rounded(deaths)}
+                                  </span>
+                                  <span
+                                    className="deathCase font-weight-bold"
+                                    style={{
+                                      fontSize: "0.6rem",
+                                    }}
+                                  >
+                                    {typeof todayDeaths === "number"
+                                      ? ` + ${rounded(todayDeaths)} new`
+                                      : "N/A"}
+                                  </span>
+                                </div>
+
+                                <div>
+                                  <span
+                                    style={{
+                                      fontSize: "0.7rem",
+                                    }}
+                                  >
+                                    Total Tests:{" "}
+                                  </span>
+                                  <span className="recoveredCase">
+                                    {rounded(tests)}
+                                  </span>
+                                </div>
+                              </PopoverContent>
+                            </Popover>
+                          }
+                        >
+                          <Geography
+                            key={geo.rsmKey}
+                            stroke="#FFF"
+                            geography={geo}
+                            // fill="#DDD"
+                            style={{
+                              default: {
+                                fill: `${colorPick(
+                                  colorCalculate(
+                                    mapSummaryData.TotalConfirmed,
+                                    cases
+                                  )
+                                )}`,
+                                stroke: "#adb5bd",
+                                strokeWidth: 0.75,
+                                outline: "none",
+                              },
+                              hover: {
+                                fill: "#F53",
+                                stroke: "#adb5bd",
+                                strokeWidth: 0.75,
+                                outline: "none",
+                              },
+                              pressed: {
+                                fill: "#E42",
+                                stroke: "#adb5bd",
+                                strokeWidth: 0.75,
+                                outline: "none",
+                              },
+                            }}
+                          />
+                        </OverlayTrigger>
+                      );
+                    })}
+
+                    {geographies.map((geo) => {
+                      const centroid = geoCentroid(geo);
+                      const cur = allStates.find((s) => s.val === geo.id);
+                      return (
+                        <g key={geo.rsmKey + "-name"}>
+                          {cur &&
+                            centroid[0] > -160 &&
+                            centroid[0] < -67 &&
+                            (Object.keys(offsets).indexOf(cur.id) === -1 ? (
+                              <Marker coordinates={centroid}>
+                                <text
+                                  y="2"
+                                  fontSize={14}
+                                  textAnchor="middle"
+                                  style={{
+                                    userSelect: "none",
+                                  }}
+                                >
+                                  {cur.id}
+                                </text>
+                              </Marker>
+                            ) : (
+                              <Annotation
+                                subject={centroid}
+                                dx={offsets[cur.id][0]}
+                                dy={offsets[cur.id][1]}
+                              >
+                                <text
+                                  x={4}
+                                  fontSize={14}
+                                  alignmentBaseline="middle"
+                                >
+                                  {cur.id}
+                                </text>
+                              </Annotation>
+                            ))}
+                        </g>
+                      );
+                    })}
+                  </>
+                )}
+              </Geographies>
+            )}
+          </ComposableMap>
+          <MapCaseSummary />
+        </>
+      ) : (
+        <MapLoading />
+      )}
+    </>
   );
 };
 
--- src/Map/WolrdMapChart.js
+++ src/Map/WolrdMapChart.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import {
   ZoomableGroup,
   ComposableMap,
@@ -7,17 +7,39 @@ import {
   Graticule,
 } from "react-simple-maps";
 
+import { Context as MapContext } from "../context/MapContext";
+
 import { OverlayTrigger, Popover, PopoverContent } from "react-bootstrap";
 import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
 
 import { rounded, abbrev, colorPick, colorCalculate } from "../helper";
 
-export const WolrdMapChart = ({
-  worldMapData,
-  GlobalTotalConfirmed,
-  loading,
-}) => {
+import { MapLoading } from "../Component/MapLoading";
+import MapCaseSummary from "./MapCaseSummary";
+
+export const WolrdMapChart = () => {
+  const [loading, setLoading] = useState(true);
   const [position, setPosition] = useState({ coordinates: [0, 0], zoom: 1 });
+
+  const {
+    state: { worldMapData, mapSummaryData },
+    fetchWorldData,
+    addWorldData,
+  } = useContext(MapContext);
+
+  useEffect(() => {
+    let fetch = async () => {
+      try {
+        let data = await fetchWorldData();
+        await addWorldData(data);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetch();
+  }, []);
 
   const handleZoomIn = () => {
     if (position.zoom >= 4) return;
@@ -35,152 +57,160 @@ export const WolrdMapChart = ({
 
   return (
     <>
-      <ComposableMap
-        projectionConfig={{ scale: 125, rotation: [-11, 0, 0] }}
-        width={750}
-        height={450}
-      >
-        <ZoomableGroup
-          zoom={position.zoom}
-          center={position.coordinates}
-          onMoveEnd={handleMoveEnd}
-        >
-          <Graticule stroke="#eeeeee" />
-          {loading && (
-            <Geographies geography={worldMapData}>
-              {({ geographies }) =>
-                geographies.map((geo) => {
-                  const {
-                    NAME,
-                    NewConfirmed,
-                    TotalConfirmed,
-                    TotalDeaths,
-                    NewDeaths,
-                    TotalRecovered,
-                    NewRecovered,
-                  } = geo.properties;
+      {!loading ? (
+        <>
+          <ComposableMap
+            projectionConfig={{ scale: 125, rotation: [-11, 0, 0] }}
+            width={750}
+            height={500}
+          >
+            <ZoomableGroup
+              zoom={position.zoom}
+              center={position.coordinates}
+              onMoveEnd={handleMoveEnd}
+            >
+              <Graticule stroke="#eeeeee" />
+              <Geographies geography={worldMapData}>
+                {({ geographies }) =>
+                  geographies.map((geo) => {
+                    const {
+                      NAME,
+                      NewConfirmed,
+                      TotalConfirmed,
+                      TotalDeaths,
+                      NewDeaths,
+                      TotalRecovered,
+                      NewRecovered,
+                    } = geo.properties;
 
-                  return (
-                    <OverlayTrigger
-                      trigger={["focus", "hover", "click"]}
-                      placement="right"
-                      key={geo.rsmKey}
-                      overlay={
-                        <Popover>
-                          <Popover.Title as="h3">{abbrev(NAME)}</Popover.Title>
-                          <PopoverContent>
-                            <div>
-                              <span
-                                style={{
-                                  fontSize: "0.7rem",
-                                }}
-                              >
-                                Total Confirmed:{" "}
-                              </span>
-                              <span className="confirmedCase">
-                                {rounded(TotalConfirmed)}
-                              </span>
-                              <span
-                                className="confirmedCase font-weight-bold"
-                                style={{
-                                  fontSize: "0.6rem",
-                                }}
-                              >
-                                {typeof NewConfirmed === "number"
-                                  ? ` + ${rounded(NewConfirmed)} new`
-                                  : "N/A"}
-                              </span>
-                            </div>
+                    return (
+                      <OverlayTrigger
+                        trigger={["focus", "hover", "click"]}
+                        placement="right"
+                        key={geo.rsmKey}
+                        overlay={
+                          <Popover>
+                            <Popover.Title as="h3">
+                              {abbrev(NAME)}
+                            </Popover.Title>
+                            <PopoverContent>
+                              <div>
+                                <span
+                                  style={{
+                                    fontSize: "0.7rem",
+                                  }}
+                                >
+                                  Total Confirmed:{" "}
+                                </span>
+                                <span className="confirmedCase">
+                                  {rounded(TotalConfirmed)}
+                                </span>
+                                <span
+                                  className="confirmedCase font-weight-bold"
+                                  style={{
+                                    fontSize: "0.6rem",
+                                  }}
+                                >
+                                  {typeof NewConfirmed === "number"
+                                    ? ` + ${rounded(NewConfirmed)} new`
+                                    : "N/A"}
+                                </span>
+                              </div>
 
-                            <div>
-                              <span
-                                style={{
-                                  fontSize: "0.7rem",
-                                }}
-                              >
-                                Total Deaths:{" "}
-                              </span>
-                              <span className="deathCase">
-                                {rounded(TotalDeaths)}
-                              </span>
-                              <span
-                                className="deathCase font-weight-bold"
-                                style={{
-                                  fontSize: "0.6rem",
-                                }}
-                              >
-                                {typeof NewDeaths === "number"
-                                  ? ` + ${rounded(NewDeaths)} new`
-                                  : "N/A"}
-                              </span>
-                            </div>
+                              <div>
+                                <span
+                                  style={{
+                                    fontSize: "0.7rem",
+                                  }}
+                                >
+                                  Total Deaths:{" "}
+                                </span>
+                                <span className="deathCase">
+                                  {rounded(TotalDeaths)}
+                                </span>
+                                <span
+                                  className="deathCase font-weight-bold"
+                                  style={{
+                                    fontSize: "0.6rem",
+                                  }}
+                                >
+                                  {typeof NewDeaths === "number"
+                                    ? ` + ${rounded(NewDeaths)} new`
+                                    : "N/A"}
+                                </span>
+                              </div>
 
-                            <div>
-                              <span
-                                style={{
-                                  fontSize: "0.7rem",
-                                }}
-                              >
-                                Total Recovered:{" "}
-                              </span>
-                              <span className="recoveredCase">
-                                {rounded(TotalRecovered)}
-                              </span>
-                              <span
-                                className="recoveredCase font-weight-bold"
-                                style={{
-                                  fontSize: "0.6rem",
-                                }}
-                              >
-                                {typeof NewRecovered === "number"
-                                  ? ` + ${rounded(NewRecovered)} new`
-                                  : "N/A"}
-                              </span>
-                            </div>
-                          </PopoverContent>
-                        </Popover>
-                      }
-                    >
-                      <Geography
-                        geography={geo}
-                        style={{
-                          default: {
-                            fill: `${colorPick(
-                              colorCalculate(
-                                GlobalTotalConfirmed,
-                                TotalConfirmed
-                              )
-                            )}`,
-                            stroke: "#adb5bd",
-                            strokeWidth: 0.75,
-                            outline: "none",
-                          },
-                          hover: {
-                            fill: "#F53",
-                            stroke: "#adb5bd",
-                            strokeWidth: 0.75,
-                            outline: "none",
-                          },
-                          pressed: {
-                            fill: "#E42",
-                            stroke: "#adb5bd",
-                            strokeWidth: 0.75,
-                            outline: "none",
-                          },
-                        }}
-                      />
-                    </OverlayTrigger>
-                  );
-                })
-              }
-            </Geographies>
-          )}
-        </ZoomableGroup>
-      </ComposableMap>
-      <div className="controls">
-        <FaPlusCircle className="controls-icons" onClick={handleZoomIn} />
-        <FaMinusCircle className="controls-icons" onClick={handleZoomOut} />
-      </div>
+                              <div>
+                                <span
+                                  style={{
+                                    fontSize: "0.7rem",
+                                  }}
+                                >
+                                  Total Recovered:{" "}
+                                </span>
+                                <span className="recoveredCase">
+                                  {rounded(TotalRecovered)}
+                                </span>
+                                <span
+                                  className="recoveredCase font-weight-bold"
+                                  style={{
+                                    fontSize: "0.6rem",
+                                  }}
+                                >
+                                  {typeof NewRecovered === "number"
+                                    ? ` + ${rounded(NewRecovered)} new`
+                                    : "N/A"}
+                                </span>
+                              </div>
+                            </PopoverContent>
+                          </Popover>
+                        }
+                      >
+                        <Geography
+                          geography={geo}
+                          style={{
+                            default: {
+                              fill: `${colorPick(
+                                colorCalculate(
+                                  mapSummaryData.TotalConfirmed,
+                                  TotalConfirmed
+                                )
+                              )}`,
+                              stroke: "#adb5bd",
+                              strokeWidth: 0.75,
+                              outline: "none",
+                            },
+                            hover: {
+                              fill: "#F53",
+                              stroke: "#adb5bd",
+                              strokeWidth: 0.75,
+                              outline: "none",
+                            },
+                            pressed: {
+                              fill: "#E42",
+                              stroke: "#adb5bd",
+                              strokeWidth: 0.75,
+                              outline: "none",
+                            },
+                          }}
+                        />
+                      </OverlayTrigger>
+                    );
+                  })
+                }
+              </Geographies>
+            </ZoomableGroup>
+          </ComposableMap>
+
+          <div className="controls">
+            <FaPlusCircle className="controls-icons" onClick={handleZoomIn} />
+            <FaMinusCircle className="controls-icons" onClick={handleZoomOut} />
+          </div>
+          {MapCaseSummary()}
+        </>
+      ) : (
+        <MapLoading />
+      )}
     </>
   );
 };
