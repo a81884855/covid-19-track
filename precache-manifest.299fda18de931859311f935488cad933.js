bf0ccf70adadb77bdc626b39803f7e02c8ff73d3
Gary <garyguan0713@gmail.com>
2020-05-25 17:00:44 -0700

    Updates

---
+++ precache-manifest.299fda18de931859311f935488cad933.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e1bf9f02b4301fbb0e5ee36a84ee64",
+    "url": "/covid-19-track/index.html"
+  },
+  {
+    "revision": "62eb08c8ec76b94a2a4d",
+    "url": "/covid-19-track/static/css/2.2a1ac492.chunk.css"
+  },
+  {
+    "revision": "52a02101f0b14f649614",
+    "url": "/covid-19-track/static/css/main.92531f1a.chunk.css"
+  },
+  {
+    "revision": "62eb08c8ec76b94a2a4d",
+    "url": "/covid-19-track/static/js/2.50a20531.chunk.js"
+  },
+  {
+    "revision": "d64bce5ad664d2419435b99b8f03ec6d",
+    "url": "/covid-19-track/static/js/2.50a20531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52a02101f0b14f649614",
+    "url": "/covid-19-track/static/js/main.3a6cc90d.chunk.js"
+  },
+  {
+    "revision": "4bec8dd171e0f143d630",
+    "url": "/covid-19-track/static/js/runtime-main.24707e95.js"
+  }
+]);
