bb1a312e35afe1a3df8665d77972e5e271937c26
Gary <garyguan0713@gmail.com>
2020-08-05 15:50:04 -0700

    create map component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Container, Row, Col } from "reactstrap";
+import axios from "axios";
+
+import Map from "./Map";
 
 function App() {
+  const [mapCountries, setMapCountries] = useState([]);
+  const [caseType, setCaseType] = useState("case");
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
+
+  const getCountriesData = async () => {
+    const { data } = axios.get("https://disease.sh/v3/covid-19/countries");
+  };
+
+  useEffect(() => {});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col md={12} lg={9}>
+            <h1>COVID-19 Tracker</h1>
+            <Map />
+          </Col>
+          <Col md={12} lg={3}>
+            sth
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
