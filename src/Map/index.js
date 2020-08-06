7046a0bfa4870c4c620ea3756887e9a7715a72d3
Gary <garyguan0713@gmail.com>
2020-08-06 15:01:35 -0700

    rename the map as Leaflemap

--- src/Map/index.js
+++ src/Map/index.js
@@ -1,27 +1,25 @@
 import React, { useContext } from "react";
-import { Map, TileLayer } from "react-leaflet";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
 import { Context as dataContext } from "../Context/dataContext";
 import { showDataOnMap } from "../utils";
 import "./index.css";
 
-const MapCompo = () => {
+const Map = () => {
   const {
     state: { mapCountries, casesType, mapCenter, mapZoom },
   } = useContext(dataContext);
 
-  console.log(mapCountries, casesType);
-
   return (
     <div className="map">
-      <Map center={mapCenter} zoom={mapZoom}>
+      <LeafletMap center={mapCenter} zoom={mapZoom}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         {showDataOnMap(mapCountries, casesType)}
-      </Map>
+      </LeafletMap>
     </div>
   );
 };
 
-export default MapCompo;
+export default Map;
