bb4e7c0b83a09a4ed9348138a312743a268364f8
Gary <garyguan0713@gmail.com>
2020-08-05 18:13:26 -0700

    impleemnt fetch countries data

--- src/Context/dataContext.js
+++ src/Context/dataContext.js
@@ -3,25 +3,41 @@ import axios from "axios";
 
 const trackReducer = (state, action) => {
   switch (action.type) {
-    case "set_loading":
+    case "fetch_countries_data":
       return {
         ...state,
+        countries: action.playload.countries,
+        mapCountries: action.playload.data,
+        tableData: action.playload.sortedData,
       };
     default:
       return {};
   }
 };
 
+const getCountriesData = (dispatch) => async () => {
+  const { data } = await axios.get("https://disease.sh/v3/covid-19/countries");
+  const countries = data.map((country) => ({
+    name: country.country,
+    valye: country.countryInfo.iso2,
+  }));
+  let sortedData = [...data].sort((a, b) => b.cases - a.cases);
+  return dispatch({
+    type: "fetch_countries_data",
+    playload: { countries, data, sortedData },
+  });
+};
+
 export const { Provider, Context } = createDataContext(
   trackReducer,
-  {},
+  { getCountriesData },
   {
     country: "worldwide",
     countryInfo: {},
     countries: [],
     mapCountries: [],
     tableData: [],
-    caseType: "case",
+    casesType: "cases",
     mapCenter: { lat: 34.80746, lng: -40.4796 },
     mapZoom: 3,
   }
