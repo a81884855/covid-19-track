e1a98fb81af4c9754a38971ac364de81aa076d2a
Gary <garyguan0713@gmail.com>
2020-08-05 18:13:44 -0700

    implement map

--- src/Map/index.js
+++ src/Map/index.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Map, TileLayer } from "react-leaflet";
+import { Context as dataContext } from "../Context/dataContext";
+import { showDataOnMap } from "../utils";
+import "./index.css";
 
-const Map = () => {
-  return <div>Map</div>;
+const MapCompo = () => {
+  const {
+    state: { mapCountries, casesType, mapCenter, mapZoom },
+  } = useContext(dataContext);
+
+  console.log(mapCountries, casesType);
+
+  return (
+    <div className="map">
+      <Map center={mapCenter} zoom={mapZoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {showDataOnMap(mapCountries, casesType)}
+      </Map>
+    </div>
+  );
 };
 
-export default Map;
+export default MapCompo;
---
+++ src/utils/index.js
@@ -0,0 +1,92 @@
+import React from "react";
+import numeral from "numeral";
+import { Circle, Popup } from "react-leaflet";
+
+const casesTypeColors = {
+  cases: {
+    hex: "#CC1034",
+    rgb: "rgb(204, 16, 52)",
+    half_op: "rgba(204, 16, 52, 0.5)",
+    multiplier: 400,
+  },
+  recovered: {
+    hex: "#7dd71d",
+    rgb: "rgb(125, 215, 29)",
+    half_op: "rgba(125, 215, 29, 0.5)",
+    multiplier: 600,
+  },
+  deaths: {
+    hex: "#fb4443",
+    rgb: "rgb(251, 68, 67)",
+    half_op: "rgba(251, 68, 67, 0.5)",
+    multiplier: 1000,
+  },
+};
+
+export const prettyPrintStat = (stat) =>
+  stat ? `+${numeral(stat).format("0.0a")}` : "+0";
+
+export const rounded = (num) => {
+  if (num > 1000000000) {
+    return Math.round(num / 100000000) / 10 + "Bn";
+  } else if (num > 1000000) {
+    return (num / 1000000).toFixed(2) + "M";
+  } else if (num > 10000) {
+    return Math.round(num / 100) / 10 + "K";
+  } else {
+    return num;
+  }
+};
+
+export const showDataOnMap = (data, casesType = "cases") =>
+  data.map((country) => (
+    <Circle
+      center={[country.countryInfo.lat, country.countryInfo.long]}
+      color={casesTypeColors[casesType].hex}
+      fillColor={casesTypeColors[casesType].hex}
+      fillOpacity={0.4}
+      radius={
+        Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+      }
+    >
+      <Popup>
+        <div className="info-container">
+          <div
+            className="info-flag"
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          ></div>
+          <div className="info-name">{country.country}</div>
+          <div className="info-confirmed">
+            Cases:
+            <div>
+              <span>{rounded(country.cases)} </span>
+              <small>
+                + {country.todayCases}
+                {" new"}
+              </small>
+            </div>
+          </div>
+          <div className="info-recovered">
+            Recovered:
+            <div>
+              <span>{rounded(country.recovered)} </span>
+              <small>
+                + {country.todayRecovered}
+                {" new"}
+              </small>
+            </div>
+          </div>
+          <div className="info-deaths">
+            Deaths:
+            <div>
+              <span>{rounded(country.deaths)} </span>
+              <small>
+                + {country.todayDeaths}
+                {" new"}
+              </small>
+            </div>
+          </div>
+        </div>
+      </Popup>
+    </Circle>
+  ));
